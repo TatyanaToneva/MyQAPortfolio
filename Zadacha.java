ec4529292fa3f32c7362d2793b936b1e0051963b
TatyanaToneva <72707232+TatyanaToneva@users.noreply.github.com>
2020-11-07 16:51:36 +0200

    Eclipse

---
+++ Zadacha.java
@@ -0,0 +1,14 @@
+//Sumirane na 3 chisla
+public class Zadacha {
+
+	public static void main(String args []) {
+		double sum = calculateTotal (5.7,2.8,8.4);
+System.out.println(sum);
+	}
+public static double calculateTotal (double x, double y, double z) {
+	 double sum = x +y +z;
+     return  sum;
+     
+     
+}
+}
