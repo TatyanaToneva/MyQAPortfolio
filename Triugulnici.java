a4400a49193516739a9c1ce7291b840ccaf838ca
TatyanaToneva <72707232+TatyanaToneva@users.noreply.github.com>
2020-11-07 14:51:09 +0200

    Eclipse

---
+++ Triugulnici.java
@@ -0,0 +1,24 @@
+// Perimeter
+import java.util.Scanner;
+
+public class Triugulnici {
+
+	;
+	
+	public static void main(String[] args) {
+		{
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Enter height"); 		
+		int height = sc.nextInt(); 		 		
+		System.out.println("Enter length"); 		
+		int length = sc.nextInt();  		
+		System.out.println("Triangle perimeter is: " + (height * length)/2); 			 	}  } 
+{
+	}
+
+}
+
+
+
+
