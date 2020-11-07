0bf4f0013b80af392649ad15adbbfd7756063b61
TatyanaToneva <72707232+TatyanaToneva@users.noreply.github.com>
2020-11-07 17:13:49 +0200

    Eclipse, Methods

---
+++ ZadachaTri.java
@@ -0,0 +1,20 @@
+
+public class ZadachaTri {
+
+	public static void main(String args []) {
+		greeting ("Ani", "Sofia");
+		greeting ("Ivan", "Kaspichan");
+	
+	}
+
+
+	
+	public static void greeting (String name, String town) {
+	System.out.println("Hello   " + name + "You are in "  + town);
+	}
+	}
+
+	
+	
+
+
